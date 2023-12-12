80dbd264524142f6a7b49d662b6f585c7507fb72
Jeonghwan Hwang <jhwang@c5r5s1.42seoul.kr>
2023-12-12 18:31:53 +0900

    practice

---
+++ src/app/read/[id]/page.js
@@ -0,0 +1,9 @@
+export default async function Read(props){
+	const id = props.params.id;
+	const resp = await fetch(`http://localhost:9999/topics/${id}`);
+	const topic = await resp.json();
+	return <>
+	  <h2>{topic.title}</h2>
+	  {topic.body}
+	</>
+  }
