32c3588f6301d2999fef3abd85c0b5af3c0d65ce
Jeonghwan Hwang <jhwang@c5r5s1.42seoul.kr>
2023-12-12 20:03:08 +0900

    test get api

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,9 +1,12 @@
-export default async function Read(props){
+export default async function Read(props) {
 	const id = props.params.id;
 	const resp = await fetch(`http://localhost:9999/topics/${id}`);
 	const topic = await resp.json();
+	const git = await fetch('https://api.github.com/repos/sunghwan2789/Bible2PPT/issues?state=all&per_page=1');
+	const res = await git.json();
+	await console.log("get api: ", res);
 	return <>
-	  <h2>{topic.title}</h2>
-	  {topic.body}
+		<h2>{topic.title}</h2>
+		{topic.body}
 	</>
-  }
+}
